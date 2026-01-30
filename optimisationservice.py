d627e440337fcd41aa229b61d1041b93e55a6964
jonat <jonathan04444@gmail.com>
2026-01-30 21:03:18 +0100

    exo avec service déja créé mais avec des trous corrigé et foncitonnel

--- optimisationservice.py
+++ optimisationservice.py
@@ -29,7 +29,7 @@
 
 services = [
     {"id": "S1", "debut": "05:30", "fin": "10:00", "voyages_assignes": [0, 1]},
-    {"id": "S2", "debut": "06:00", "fin": "10:30", "voyages_assignes": [3]},
+    {"id": "S2", "debut": "06:00", "fin": "10:30", "voyages_assignes": [2]},
     {"id": "S3", "debut": "06:00", "fin": "11:00", "voyages_assignes": []},
 ]
 
@@ -51,14 +51,18 @@ voyages = [
     ("L3", 3, "GARE", "DEPOT", "08:00", "08:30"),  # v8 → ?
 ]
 
-pause_min = 10
+from ortools.sat.python import cp_model
+
+model = cp_model.CpModel()
+
+pause_min = 5
 
 class Service:
-    def __init__(self, id, debut, fin, voyages_assignes):
+    def __init__(self, id, debut, fin):
         self.id = id
         self.debut = self.time_to_minutes(debut)
         self.fin = self.time_to_minutes(fin)
-        self.voyages_assignes = voyages_assignes
+        self.voyages_assignes = []
 
     def time_to_minutes(self, heure_str):
         h, m = heure_str.split(':')
@@ -88,8 +92,8 @@ class Voyage:
         return f"{h:02d}:{m:02d}"
 
 def chevauchement(v1, v2):
-    return voyages_objets[v1].heure_fin + pause_min > voyages_objets[v2].heure_debut and \
-           voyages_objets[v2].heure_fin + pause_min > voyages_objets[v1].heure_debut
+    return voyages_objets[v1].h_fin + pause_min > voyages_objets[v2].h_debut and \
+           voyages_objets[v2].h_fin + pause_min > voyages_objets[v1].h_debut
 
 voyages_objets = []
 
@@ -97,8 +101,51 @@ for voyage in voyages:
     voyages_objets.append(Voyage(*voyage))
 
 services_objets = []
-for service in services:
-    services_objets.append(Service(service["id"], service["debut"], service["fin"], service["voyages_assignes"]))
+for s in services:
+    serv = Service(s["id"], s["debut"], s["fin"])
+    serv.voyages_assignes = s["voyages_assignes"]
+    services_objets.append(serv)
 
-for v in services_objets:
-    print(v.id, v.debut, v.fin, v.voyages_assignes)
+x = {}
+for v in range(len(voyages_objets)):
+    for s in range(len(services_objets)):
+        x[v, s] = model.NewBoolVar(f"voyage_{v}_service_{s}")
+
+for v in range(len(voyages_objets)):
+    model.Add(sum(x[v, s] for s in range(len(services_objets))) == 1)
+
+for s in range(len(services_objets)):
+    for v in services_objets[s].voyages_assignes:
+        model.Add(x[v, s] == 1)
+
+for v1 in range(len(voyages_objets)):
+    for v2 in range(v1 + 1, len(voyages_objets)):
+        if chevauchement(v1, v2):
+            for s in range(len(services_objets)):
+                model.Add(x[v1, s] + x[v2, s] <= 1)
+
+solver = cp_model.CpSolver()
+status = solver.Solve(model)
+
+if status == cp_model.OPTIMAL:
+    for s in range(len(services_objets)):
+        serv = services_objets[s]
+        print(f"\n=== Service {serv.id} ({serv.minutes_to_time(serv.debut)} - {serv.minutes_to_time(serv.fin)}) ===")
+        for v in range(len(voyages_objets)):
+            if solver.Value(x[v, s]) == 1:
+                voy = voyages_objets[v]
+                debut = voy.minutes_to_time(voy.h_debut)
+                fin = voy.minutes_to_time(voy.h_fin)
+
+                # Marquer si pré-assigné ou ajouté
+                if v in serv.voyages_assignes:
+                    tag = "[FIXE]"
+                else:
+                    tag = "[AJOUTÉ]"
+
+                print(f"  {tag} {voy.ligne}-{voy.num}: {voy.debut} → {voy.fin} ({debut} - {fin})")
+
+elif status == cp_model.INFEASIBLE:
+    print("❌ Pas de solution possible")
+else:
+    print(f"Status: {status}")
